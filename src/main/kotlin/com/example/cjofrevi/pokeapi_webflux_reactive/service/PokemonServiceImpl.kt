ec01d980376ee3ebd548c7c29000f01d5e6a54f2
megalocoman <cfjofre@uc.cl>
2025-06-13 14:00:47 -0400

    more commits

--- src/main/kotlin/com/example/cjofrevi/pokeapi_webflux_reactive/service/PokemonServiceImpl.kt
+++ src/main/kotlin/com/example/cjofrevi/pokeapi_webflux_reactive/service/PokemonServiceImpl.kt
@@ -13,6 +13,8 @@ import org.springframework.web.reactive.function.client.bodyToFlux
 import org.springframework.web.reactive.function.client.bodyToMono
 import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
+import reactor.kotlin.core.publisher.toMono
+import java.util.Optional
 
 
 @Service
@@ -23,26 +25,34 @@ class PokemonServiceImpl(
 ) : PokemonService {
 
     override fun getPokemonList(): Flux<PokemonList> {
-        return getPokemonList(1)
-            .flatMap { resultPokeList -> getPokemonList(resultPokeList.count).log() }
-            .flatMap { resultPokeList -> save(resultPokeList).log() }
+        val path = "/pokemon"
+        return callPokeApi(path, "1").bodyToFlux<ResultPokeList>()
+            .flatMap { resultPokeList -> callPokeApi(path, resultPokeList.count.toString())
+                .bodyToFlux<ResultPokeList>()}
+            .flatMap { resultPokeList -> save(resultPokeList) }
     }
 
     override fun getPokemonListFromDB(): Flux<PokemonList> = repository.findAll()
 
     override fun getPokemonByName(name: String): Mono<Pokemon> =
         repository.findByName(name)
-            .flatMap { result -> getPokemonByNameApi(result.name) }
-            .flatMap { result -> pokemonRepository.save(result)
+            .flatMap { result -> callPokeApi("/pokemon/$result.name", null)
+                .bodyToMono<Pokemon>() }
+            .flatMap { result ->
+                pokemonRepository.save(result)
             }
 
     override fun fetchAndSaveAllPokemon(): Mono<String> =
         repository.findAll()
-            .flatMap { resultList -> getPokemonByNameApi(resultList.name)}
-            .flatMap { pokemon -> pokemonRepository.save(pokemon)  }
-            .then<String>(   Mono.just<String>("fin")
+            .flatMap { resultList ->
+                val path: String = "/pokemon/"+ resultList.name
+                callPokeApi(path, null)
+                    .bodyToMono<Pokemon>()
+            }.onErrorContinue { error, response -> println("Pokemon $response had a problem $error")  }
+            .flatMap { pokemon -> pokemonRepository.save(pokemon) }
+            .then<String>(
+                Mono.just<String>("fin")
             )
-
 
     private fun save(result: ResultPokeList): Flux<PokemonList> {
 
@@ -55,37 +65,18 @@ class PokemonServiceImpl(
         return repository.insert<PokemonList>(fluxPokemonListEntity)
     }
 
-    private fun getPokemonListJson(limit: Int): Mono<String> =
-        client.get()
+    private fun callPokeApi(path: String, limitVal: String?): WebClient.ResponseSpec {
+
+        val limitOptional: Optional<String> = Optional.ofNullable(limitVal)
+        return client.get()
             .uri { uriBuilder ->
                 uriBuilder
-                    .path("/pokemon")
-                    .queryParam("limit", limit)
+                    .path(path)
+                    .queryParamIfPresent("limit", limitOptional)
                     .queryParam("offset", 0)
                     .build()
-            }
-            .retrieve()
-            .bodyToMono<String>()
+            }.retrieve()
 
-    private fun getPokemonList(limit: Int): Flux<ResultPokeList> =
-        client.get()
-            .uri { uriBuilder ->
-                uriBuilder
-                    .path("/pokemon")
-                    .queryParam("limit", limit)
-                    .queryParam("offset", 0)
-                    .build()
-            }
-            .retrieve()
-            .bodyToFlux<ResultPokeList>()
 
-    private fun getPokemonByNameApi(name: String): Mono<Pokemon> =
-        client.get()
-            .uri { uriBuilder ->
-                uriBuilder
-                    .path("/pokemon/{name}")
-                    .build(name)
-            }
-            .retrieve()
-            .bodyToMono<Pokemon>()
+    }
 }
